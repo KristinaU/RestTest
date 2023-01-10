50ae56bd6399b3d88107be0063e43d2739d7dae5
Kristina Urosova <urosova.k@gmail.com>
2023-01-10 11:54:35 +0000

    Initial Commit

---
+++ RestTest/RestTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestTestApp.swift
+//  RestTest
+//
+//  Created by KRISTINA UROSOVA on 10/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
